c50e4ecdfc77f5e4a19e8aaa9db2cc725d66731c
scholar <andre.bernardino@tecnico.ulisboa.pt>
2017-05-10 12:08:58 +0100

    profile layout

--- app/src/main/java/com/example/eduardo/locmess/ProfileFragment.java
+++ app/src/main/java/com/example/eduardo/locmess/ProfileFragment.java
@@ -1,18 +1,28 @@
 package com.example.eduardo.locmess;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ProfileFragment extends Fragment {
 
     public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return rootView;
     }
 
 }
